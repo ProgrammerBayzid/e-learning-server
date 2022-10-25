50a1a07e4c223de38235f31b410092abf9d951d6
ProgrammerBayzid <mdbayzid2622002@gmail.com>
2022-10-25 18:07:09 +0600

    server first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const app = express();
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+app.use(cors());
+
+
+const courses = require('./Data/courses.json')
+
+
+
+
+app.get('/', (req, res) => {
+    res.send('News API Running')
+})
+
+app.get('/courses', (req, res) => {
+    res.send(courses)
+})
+app.get("/courses/:id", (req, res) => {
+    const id = req.params.id;
+    const singleCourses = courses.find(n => n._id === id)
+    res.send(singleCourses)
+})
+
+app.listen(port, () => {
+    console.log('News Server Running On Port', port);
+})
